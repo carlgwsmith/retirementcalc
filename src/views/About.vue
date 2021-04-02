134513f901c8f16ebf014f11632d963475d85e97
carlvskansas <48308402+carlvskansas@users.noreply.github.com>
2021-04-01 22:59:32 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="about">
-    <h1>This calculator is designed to give the user an idea of some simple retirement visualizations.</h1>
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>This calculator is designed to give the user an idea of some simple retirement visualizations.</h1>
+    <h2>Interested in using this for your business or want me to make something for you? <a href="mailto:carlgwsmith@gmail.com">Email me</a></h2>
     <h3>This application was created to demonstrate proficiency in VueJS, Vuex, ChartJS, and combining input to present a compelling visual presentation.</h3>
-    <p>Interested in using this for your business? <a href="mailto:carlgwsmith@gmail.com">Email me</a></p>
-    <h6>This is not financial advice.</h6>
+    <hr>
+    <h6>I'm not a financial advisor and this is not financial advice.</h6>
     <h6 class="disclosure">This illustration is hypothetical and does not represent the return on any particular investment. The rate is not guaranteed. All investing is subject to risk, including the possible loss of the money you invest.</h6>
     <h6 class="disclosure">Information and interactive calculators are made available to you as educational tools for your independent use and are not intended to provide financial planning or investment advice. These tools help you see which factors are most important to consider in making a particular financial decision, and they illustrate the relative impact of each factor on the projected outcome.</h6>
     <h6 class="disclosure">All examples in these tools are hypothetical and for illustrative purposes only. We cannot and do not guarantee the accuracy of the results or their applicability to your individual circumstances.</h6>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped>
+.about{
+  padding:5%;
+  border:
+}
+h1{
+  font-size:2em;
+}
+h2{
+  padding-top:30px;
+  font-size:1.5em;
+}
 h3{
-  padding-top:20px
+  padding-top:20px;
+  padding-bottom: 20px;
+  font-size: 1em;
 }
 h6:first-of-type{
-  padding-top:20px !important;
+  padding-top:10px !important;
 }
 p{
   padding-top:20px;
